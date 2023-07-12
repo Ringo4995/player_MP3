5ffed557f5d78f5855cc2cd60bf5018fbc3271ae
Vincent <will.beyaert@gmail.com>
2023-07-12 16:56:51 +0200

    ajout de ce qu'on a fait le mercredi en cours

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// lorsqu'on importe un element ( ou plusieurs ), je le fais en début du script
+import { playlist_hiphop } from "./lib/playlist_hiphop.js";
+console.dir(playlist_hiphop);
+console.log("Hello");
