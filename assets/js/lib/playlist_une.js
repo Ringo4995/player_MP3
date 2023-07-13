cb4d9d0c2864770f69075c66cd60d10193d6d4ea
Ringo4995 <will.beyaert@gmail;com>
2023-07-13 17:51:32 +0200

    ajout de la foad

---
+++ assets/js/lib/playlist_une.js
@@ -0,0 +1,32 @@
+const playlist_une = [
+    {
+        "title" : "Davy Jones",
+        "author": "Fia Orädd",
+        "cover" : "./assets/img/cover/fiaoradd.jpg",// aller chercher l'image avec l'url depuis l'index, ne pas oublier les points
+        "year" : 2017,
+        "audio" : "./assets/audio/Davy Jones [Lyrics].mp3",
+        "genre" : ['Classic','Movie'],
+        "description" : "Une réinterprétation du thème de Davy Jones, mais avec des paroles écrites pour l'occasion.",
+    },
+    {
+        "title" : "Catch The Rainbow",
+        "author": "Rainbow",
+        "cover" : "./assets/img/cover/Rainbow.jpg",
+        "year" : 1975,
+        "audio" : "./assets/audio/Catch The Rainbow.mp3",
+        "genre" : ['Hard rock melodique'],
+        "description" : "Une des chansons du premier album du groupe Rainbow qui révélera le talent d'un certain Dio.",
+    },
+    {
+        "title" : "The Wonder Of You",
+        "author": "Elvis Presley",
+        "cover" : "./assets/img/cover/Elvis_The_Wonder_Of_You.jpeg",
+        "year" : 1970,
+        "audio" : "./assets/audio/The Wonder of You.mp3",
+        "genre" : ['country','pop'],
+        "description" : "Écrite par Baker Knight, la chanson fût originellement enregistrée par Ray Peterson en 1959, en 1970 en sort l'interprétation la plus célèbre en single, elle devient plus tard une des chansons qu'il chante fréquemment en concert.",
+    },
+    
+]
+
+export{playlist_une, }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
 // lorsqu'on importe un element ( ou plusieurs ), je le fais en début du script
-import { playlist_hiphop } from "./lib/playlist_hiphop.js";
-console.dir(playlist_hiphop);
+import { playlist_une } from "./lib/playlist_une.js";
+console.dir(playlist_une);
 console.log("Hello");
